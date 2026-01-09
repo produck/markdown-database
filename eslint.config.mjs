65a8365b11d9a5d7390ec11cdf408832255aa779
chaoslee <chaos2lee@qq.com>
2026-01-09 13:55:59 +0800

    workspace: [FIXED] lint: with globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import js from '@eslint/js';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 import * as ProduckEslint from '@produck/eslint-rules';
@@ -8,6 +9,7 @@ export default defineConfig([
 		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
 		plugins: { js },
 		extends: ['js/recommended'],
+		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 	},
 	tseslint.configs.recommended,
 	ProduckEslint.config,
